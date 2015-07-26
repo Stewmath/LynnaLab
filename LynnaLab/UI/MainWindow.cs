8b990205b997b9dbb4b16f8cec0cd081291ed3d6
Matthew Stewart <stewartmatthew6@gmail.com>
2015-07-26 00:46:02 -0400

    Move open project warning

--- LynnaLab/UI/MainWindow.cs
+++ LynnaLab/UI/MainWindow.cs
@@ -11,35 +11,49 @@ public partial class MainWindow: Gtk.Window
 	{
 		Build();
 
-		OpenProject("/home/matthew/programs/gb/ages/ages-disasm");
-
-        roomeditor1.SetRoom(new Room(project, 0x48));
-        areaviewer1.SetArea(roomeditor1.Room.Area);
+		OpenProject("/home/matthew/programs/gb/ages/aages-disasm");
 	}
 
 	void OpenProject(string dir) {
-		if (project != null) {
-			project.Close();
-			project = null;
-		}
-		project = new Project(dir);
-		/*
-		try {
-			project = new Project(dir);
-		}
-		catch (Exception ex) {
-			string outputString = "The following error was encountered while opening the project:\n\n";
-			outputString += ex.Message;
+        ResponseType response = ResponseType.Yes;
+        if (!File.Exists(dir + "/main.s")) {
+            Gtk.MessageDialog d = new MessageDialog(this,
+                                      DialogFlags.DestroyWithParent,
+                                      MessageType.Warning,
+                                      ButtonsType.YesNo,
+                                      "The folder you selected does not have a main.s file. This probably indicates the folder does not contain the ages disassembly. Attempt to continue anyway?");
+            response = (ResponseType)d.Run();
+            d.Destroy();
+        }
 
-			Gtk.MessageDialog d = new MessageDialog(this,
-				                     DialogFlags.DestroyWithParent,
-				                     MessageType.Error,
-				                     ButtonsType.Ok,
-				                     outputString);
-			d.Run();
-			d.Destroy();
-		}
-*/
+        if (response == ResponseType.Yes) {
+            if (project != null) {
+                project.Close();
+                project = null;
+            }
+            project = new Project(dir);
+
+            /*
+            try {
+                project = new Project(dir);
+            }
+            catch (Exception ex) {
+                string outputString = "The following error was encountered while opening the project:\n\n";
+                outputString += ex.Message;
+
+                Gtk.MessageDialog d = new MessageDialog(this,
+                                         DialogFlags.DestroyWithParent,
+                                         MessageType.Error,
+                                         ButtonsType.Ok,
+                                         outputString);
+                d.Run();
+                d.Destroy();
+            }
+    */
+
+            roomeditor1.SetRoom(new Room(project, 0x48));
+            areaviewer1.SetArea(roomeditor1.Room.Area);
+        }
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
@@ -61,19 +75,7 @@ public partial class MainWindow: Gtk.Window
 
 		if (response == ResponseType.Accept) {
 			string basedir = dialog.Filename;
-			response = ResponseType.Yes;
-			if (!File.Exists(basedir + "/main.s")) {
-				Gtk.MessageDialog d = new MessageDialog(this,
-					                      DialogFlags.DestroyWithParent,
-					                      MessageType.Warning,
-					                      ButtonsType.YesNo,
-					                      "The folder you selected does not have a main.s file. This probably indicates the folder does not contain the ages disassembly. Attempt to continue anyway?");
-				response = (ResponseType)d.Run();
-				d.Destroy();
-			}
-			if (response == ResponseType.Yes) {
-				OpenProject(basedir);
-			}
+			OpenProject(basedir);
 		}
 		dialog.Destroy();
 	}
