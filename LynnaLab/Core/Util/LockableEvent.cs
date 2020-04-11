25b6064c9808d97d1f9d3bd1aeee2c43c08b65a6
Matthew Stewart <stewartmatthew6@gmail.com>
2020-04-11 11:43:17 -0400

    Save all missed events in LockableEvent class

--- LynnaLab/Core/Util/LockableEvent.cs
+++ LynnaLab/Core/Util/LockableEvent.cs
@@ -1,26 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 namespace LynnaLab {
     // This is just like an Event, but you can "Lock" it to pause callbacks, and "Unlock" it to
     // resume them. Useful for doing atomic operations.
-    // NOTE: This assumes that only the most recent event matters. If multiple events occur while
-    // locked, all but the last one will be skipped.
     public class LockableEvent<T> {
         EventHandler<T> handler;
 
         int locked = 0;
-        bool missedInvoke = false;
-        object invokeSender;
-        T invokeParam;
+
+        List<Tuple<object,T>> savedInvokes = new List<Tuple<object,T>>();
 
         public LockableEvent() {
         }
 
         public void Invoke(object sender, T args) {
             if (locked != 0) {
-                missedInvoke = true;
-                invokeSender = sender;
-                invokeParam = args;
+                savedInvokes.Add(new Tuple<object,T>(sender, args));
             }
             else {
                 if (handler != null)
@@ -36,16 +32,19 @@ namespace LynnaLab {
             if (locked == 0)
                 throw new Exception("Called Unlock on an already unlocked LockableEvent.");
             locked--;
-            if (locked == 0 && missedInvoke) {
-                Invoke(invokeSender, invokeParam);
-                missedInvoke = false;
+            if (locked == 0) {
+                foreach (var t in savedInvokes) {
+                    if (handler != null)
+                        handler(t.Item1, t.Item2);
+                }
+                savedInvokes.Clear();
             }
         }
 
         // If an event triggered while locked, this clears the event to prevent it from running when
         // Unlock() is called.
         public void Clear() {
-            missedInvoke = false;
+            savedInvokes.Clear();
         }
 
 
