7fd0434bf7c18fda0453b06ee07b813443f3caeb
Matthew Stewart <stewartmatthew6@gmail.com>
2020-09-06 14:04:58 -0400

    Fixed log4net

--- LynnaLab/LogHelper.cs
+++ LynnaLab/LogHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using log4net;
 using System.Reflection;
 
@@ -6,6 +7,14 @@ namespace LynnaLab
 {
     public class LogHelper
     {
+        /// When using .NET framework this was not necessary. But after switching to .NET Core it
+        /// became necessary to add this static constructor which loads the log4net config.
+        static LogHelper() {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            log4net.Config.XmlConfigurator.Configure(logRepository, new System.IO.FileInfo("log4net.config"));
+        }
+
+
         public static void AddAppenderToRootLogger(log4net.Appender.IAppender a) {
             ((log4net.Repository.Hierarchy.Hierarchy)LogManager.GetRepository(Assembly.GetEntryAssembly())).Root.AddAppender(a);
         }
