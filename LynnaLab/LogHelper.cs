87044bfc4db3dab410bcb1ec5e389c95877b7722
Matthew Stewart <stewartmatthew6@gmail.com>
2020-09-05 19:44:00 -0700

    Ported to .NET Core

--- LynnaLab/LogHelper.cs
+++ LynnaLab/LogHelper.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using log4net;
+using System.Reflection;
 
 namespace LynnaLab
 {
     public class LogHelper
     {
         public static void AddAppenderToRootLogger(log4net.Appender.IAppender a) {
-            ((log4net.Repository.Hierarchy.Hierarchy)LogManager.GetRepository()).Root.AddAppender(a);
+            ((log4net.Repository.Hierarchy.Hierarchy)LogManager.GetRepository(Assembly.GetEntryAssembly())).Root.AddAppender(a);
         }
 
         public static void RemoveAppenderFromRootLogger(log4net.Appender.IAppender a) {
-            ((log4net.Repository.Hierarchy.Hierarchy)LogManager.GetRepository()).Root.RemoveAppender(a);
+            ((log4net.Repository.Hierarchy.Hierarchy)LogManager.GetRepository(Assembly.GetEntryAssembly())).Root.RemoveAppender(a);
         }
     }
 }
