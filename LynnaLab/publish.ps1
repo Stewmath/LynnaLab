571aeba097247cfb48f0900dd644b991958108cf
Matthew Stewart <stewartmatthew6@gmail.com>
2020-09-07 10:56:13 -0700

    Updated publishing script to create a portable build

--- LynnaLab/publish.ps1
+++ LynnaLab/publish.ps1
@@ -1,12 +1,20 @@
-# Publish windows build.
+# Publish a portable build which also includes Windows GTK dependencies.
 # GTK libs should already be in the LocalAppData directory from GtkSharp's automated download.
 
-dotnet publish /p:PublishProfile=Properties\PublishProfiles\win-x64.pubxml
+# WINDOWS
+#==========================================================================
+dotnet publish /p:PublishProfile=Properties\PublishProfiles\portable.pubxml
 
 $gtkdir="$env:LOCALAPPDATA\Gtk\3.24.20\"
 $publishbasedir="bin\Release\Publish\"
-$publishdir="$publishbasedir\LynnaLab-win64"
+$publishdir="$publishbasedir\LynnaLab"
+$gitversion=cat version.txt
 
+# Copy over Windows GTK libraries. For other platforms (linux, mac) GTK must be pre-installed.
+echo "Copying GTK libraries to release directory..."
 cp -Path @("$gtkdir\*.dll", "$gtkdir\etc", "$gtkdir\gtk3-runtime", "$gtkdir\lib", "$gtkdir\share", "$gtkdir\ssl") `
    -Destination $publishdir -Force -Recurse
-Compress-Archive -LiteralPath $publishdir -DestinationPath $publishbasedir\LynnaLab-win64.zip -Force
+   
+# Zip it
+echo "Compressing the archive..."
+Compress-Archive -LiteralPath $publishdir -DestinationPath $publishbasedir\LynnaLab-$gitversion.zip -Force
