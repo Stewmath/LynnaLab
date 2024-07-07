f3afb4faa65608da144744243307927199f51978
Matthew Stewart <stewartmatthew6@gmail.com>
2024-07-06 21:25:15 -0400

    Removed LynnaLab.bat

--- LynnaLab/LynnaLab.bat
+++
@@ -1,5 +0,0 @@
-﻿REM     Because GTK's file chooser seems to have a hard time locating WSL files, this batch script can be used
-REM     to feed the path to the disassembly directly to LynnaLab.
-REM     Type "explorer.exe ." in a ubuntu terminal to open explorer, then press "Ctrl-L" to get the path to that folder.
-REM     Copy that path here (replace everything after "LynnaLab.exe").
-LynnaLab.exe \\wsl$\Ubuntu\home\<REPLACE WITH LINUX USERNAME>\oracles-disasm
