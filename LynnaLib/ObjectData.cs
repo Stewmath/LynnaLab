10f5418a4a9f71224bfcce695c650419006b4350
Matthew Stewart <stewartmatthew6@gmail.com>
2024-07-08 21:53:47 -0400

    Fixed warning about obj_Garbage

--- LynnaLib/ObjectData.cs
+++ LynnaLib/ObjectData.cs
@@ -51,11 +51,11 @@ namespace LynnaLib
 
         // # of parameters for each opcode, used by FileParser.cs
         public static int[] ObjectCommandMinParams = {
-            1,  2,  1,  1,  1,  3,  4,  5,  3,  2,  0,  0, 2
+            1,  2,  1,  1,  1,  3,  4,  5,  3,  2,  0,  0, 1
         };
 
         public static int[] ObjectCommandMaxParams = {
-            -1, 5, -1, -1, -1, -1,  5, -1,  5,  3, -1, -1, 2
+            -1, 5, -1, -1, -1, -1,  5, -1,  5,  3, -1, -1, 4
         };
 
         public static int[] ObjectCommandDefaultParams = {
