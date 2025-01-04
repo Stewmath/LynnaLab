b811bb5c6a88fd8ffef9cfc55ea6e0bc8754565a
Matthew Stewart <stewartmatthew6@gmail.com>
2025-01-03 20:02:47 -0500

    Removed old unused scripts

--- LynnaLab/package.sh
+++
@@ -1,5 +0,0 @@
-# User must have .NET Core installed
-dotnet publish -c Release -r win-x64 --self-contained false /p:PublishSingleFile=true
-
-# Packages all of .NET Core with the application
-#dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishSingleFile=true
