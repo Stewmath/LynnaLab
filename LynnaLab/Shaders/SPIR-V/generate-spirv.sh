42ed90f89f3c24d7ec22560d4c4bb2cc83ea29d7
Matthew Stewart <stewartmatthew6@gmail.com>
2025-05-09 17:08:01 -0400

    General: Add build rules for SPIR-V shaders
    Linux only; should be ignored on other systems.

--- LynnaLab/Shaders/SPIR-V/generate-spirv.sh
+++ LynnaLab/Shaders/SPIR-V/generate-spirv.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-glslangValidator -V imgui-vertex.glsl -o imgui-vertex.spv -S vert
-glslangValidator -V imgui-frag.glsl -o imgui-frag.spv -S frag
-glslangValidator -V tileset-vertex.glsl -o tileset-vertex.spv -S vert
-glslangValidator -V tileset-frag.glsl -o tileset-frag.spv -S frag
+glslangValidator -V imgui-vertex.glsl -o imgui-vertex.spv -S vert     || exit 1
+glslangValidator -V imgui-frag.glsl -o imgui-frag.spv -S frag         || exit 1
+glslangValidator -V tileset-vertex.glsl -o tileset-vertex.spv -S vert || exit 1
+glslangValidator -V tileset-frag.glsl -o tileset-frag.spv -S frag     || exit 1
